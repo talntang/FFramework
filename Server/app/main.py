d8aa6ca8849fd738a13834b93300e6580de9e473
fengyunlong <manoking@126.com>
2025-03-06 16:47:17 +0800

    移除无效注释

--- Server/app/main.py
+++ Server/app/main.py
@@ -18,14 +18,3 @@ async def root():
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
-
-
-
-'''创建一个fastapi程序
-（1）导入 FastAPI。
-（2）创建一个 app 实例。
-（3）编写一个路径操作装饰器（如 @app.get("/")）。
-（4）编写一个路径操作函数（如上面的 def root(): ...）
-（5）定义返回值
-（6）运行开发服务器（如 uvicorn main:app --reload）
-'''
